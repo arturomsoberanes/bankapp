8d6a7cc7a1f6284372af4dfbccfda025af45f7b3
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-12-05 18:04:35 -0600

    Add Swagger UI for Docmentation for API

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const cors = require('cors')
 const user = require('./src/routes/users')
 const movements = require('./src/routes/movements')
 const { connection } = require('./src/bd-connection')
+// Swagger Api Documentation
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
 const port = 4000;
 
 // Used to serve static files from public directory
 app.use(express.static('public/build'));
 app.use(cors());
+
+// Swagger End-Point
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // Connect to bd
 connection()
